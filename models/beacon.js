42baf48613f961be7307d5bb082cf82320e263fd
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-08-16 13:42:51 -0400

    initial tweaks

---
+++ models/beacon.js
@@ -0,0 +1,11 @@
+module.exports = function (sequelize, DataTypes) {
+  var Beacon = sequelize.define("Beacon", {
+    title: DataTypes.STRING,
+    category: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    address: DataTypes.STRING,
+    startTime: DataTypes.TIME,
+    date: DataTypes.DATE
+  });
+  return Beacon;
+};
