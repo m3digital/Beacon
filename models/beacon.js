049af036c49cd2a22a2e210d066444274af02547
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-08-16 22:22:53 -0400

    added passport npms, added passport middleware for checking if users are authenticated when they make get requests, passport function for apiroutes, developed our user model, took a solid first crack at html routes, a more confused overall crack at api routes, added necessary requires to server.js

--- models/beacon.js
+++ models/beacon.js
@@ -1,4 +1,4 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var Beacon = sequelize.define("Beacon", {
     title: DataTypes.STRING,
     category: DataTypes.STRING,
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,62 @@
+// TDB: Just noting that I've removed the requiring of path from the passport example, because I think using
+// handlebars means we don't need them. I might be wrong about that.
+
+// Requiring our middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // The current structure I'm working with is:
+  // "/" for landing page, which links to a sign in page and a log in page
+  // "/login" for login page
+  // "/signup" for signup page
+  // "/events" for the main page you see once you're authenticated
+  // Cassidy and Max, feel free to change any of these titles to whatever you're comfortable with.
+
+  // If the user already has an account, persistent login from passport.js sends them to the events page
+  // Otherwise they go to the landing page
   app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/events");
+    }
+    res.render("landing", {});
+  });
+
+  // If the user already has an account, persistent login from passport.js sends them to the events page
+  // Otherwise they go to the landing page
+  app.get("/login", function(req, res) {
+    if (req.user) {
+      res.redirect("/events");
+    }
+    res.render("login", {});
+  });
+
+  // If the user already has an account, persistent login from passport.js sends them to the events page
+  // Otherwise they go to the landing page
+  app.get("/signup", function(req, res) {
+    if (req.user) {
+      res.redirect("/events");
+    }
+    res.render("signup", {});
+  });
+
+  // We'll use the isAuthenticated middleware in all other get requests to redirect users to the landing page
+  // if they aren't logged in.
+
+  app.get("/events", isAuthenticated, function(req, res) {
     db.Beacon.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Beacon.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", isAuthenticated, function(req, res) {
+    db.Beacon.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
