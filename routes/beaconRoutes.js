e68e3ca167d375e586096e31f82ee2c47de7ec34
Max Szczepaniak <wookslandline@protonmail.com>
2019-08-20 14:25:57 -0400

    adds the geocode formated address to the SQL database upon beacon creation

--- routes/beaconRoutes.js
+++ routes/beaconRoutes.js
@@ -28,6 +28,7 @@ module.exports = function(app) {
       },
       function(err, response) {
         if (!err) {
+          newBeacon.address = response.json.results[0].formated_address;
           newBeacon.latitude = response.json.results[0].geometry.location.lat;
           newBeacon.longitude = response.json.results[0].geometry.location.lng;
           db.Beacon.create(newBeacon).then(function(dbBeacon) {
