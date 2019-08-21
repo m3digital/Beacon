e45fb83bf282b193e68f9cba1584dc84f5b63037
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-08-21 10:01:55 -0400

    rearranged edit-delete file contents to beacon.js and comment.js, and updated handlebar files to call for beacon.js

---
+++ public/js/beacon.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  var beaconForm = $("#form-beacon");
+  var titleInput = $("#title");
+  var categoryInput = $("#category");
+  var descriptionInput = $("#desc");
+  var addressInput = $("#address");
+  var startTimeInput = $("#start-time");
+  var endTimeInput = $("#end-time");
+  var dateInput = $("#date");
+
+  beaconForm.on("submit", function(event) {
+    event.preventDefault();
+    var beaconData = {
+      title: titleInput.val().trim(),
+      category: categoryInput.val().trim(),
+      description: descriptionInput.val().trim(),
+      address: addressInput.val().trim(),
+      startTime: startTimeInput.val().trim(),
+      endTime: endTimeInput.val().trim(),
+      date: dateInput.val().trim()
+    };
+    newBeacon(beaconData);
+  });
+
+  function newBeacon(object) {
+    $.post("/api/beacons", object).then(function(data) {
+      var id = data.id;
+      console.log(data);
+      window.location.assign("/beacon/" + id);
+    });
+  }
+
+  function deleteBeacon() {
+    var thisBeaconID = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/beacons/" + thisBeaconID
+    })
+      .then(function() {
+        window.location.replace("/browse");
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  function editBeacon() {
+    var titleInput = $("#title");
+    var categoryInput = $("#category");
+    var descriptionInput = $("#desc");
+    var addressInput = $("#address");
+    var startTimeInput = $("#start-time");
+    var endTimeInput = $("#end-time");
+    var dateInput = $("#date");
+
+    var newVersion = {
+      title: titleInput.val().trim(),
+      category: categoryInput.val().trim(),
+      description: descriptionInput.val().trim(),
+      address: addressInput.val().trim(),
+      startTime: startTimeInput.val().trim(),
+      endTime: endTimeInput.val().trim(),
+      date: dateInput.val().trim()
+    };
+    $.ajax({
+      method: "PUT",
+      url: "/api/beacons",
+      data: newVersion
+    }).then(function(data) {
+      var id = data.id;
+      console.log(data);
+      window.location.assign("/beacon/" + id);
+    });
+  }
+
+  $(".beacon-buttons").on("click", ".btn-delete", deleteBeacon);
+  $(".beacon-buttons").on("click", ".btn-edit", editBeacon);
+});
