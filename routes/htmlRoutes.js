9dff934baa07244085f31ab32713f2169802e52d
Cassidy G <cassidygroenendaal@gmail.com>
2019-08-21 13:20:06 -0400

    Update GOOGLEAPI key, Adjust BeaconId retrieval from URL so that it grabs the correct part of the URL

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -63,7 +63,7 @@ module.exports = function(app) {
     db.Beacon.findAll({}).then(function(foundBeacons) {
       res.render("browse", {
         foundBeacons: foundBeacons,
-        apiKey: process.env.keyskeys
+        apiKey: process.env.GOOGLEAPI
       });
     });
   });
